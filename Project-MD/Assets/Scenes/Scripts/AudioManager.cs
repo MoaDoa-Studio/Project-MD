43cbfca3e5807a37201120d04d5c9833743c4ba8
MooniMakey <80091657+moonimax@users.noreply.github.com>
2023-11-21 12:04:50 +0900

    11

--- Project-MD/Assets/Scenes/Scripts/AudioManager.cs
+++ Project-MD/Assets/Scenes/Scripts/AudioManager.cs
@@ -7,9 +7,11 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
-    private AudioMixer audioMixer;
+    public AudioMixer audioMixer;
     public Sound[] sounds;
-    
+
+    // 플레이 배경음 이름
+    public string bgmName = "";
     private void Awake()
     {
         if(instance != null)
@@ -50,7 +52,7 @@ public class AudioManager : MonoBehaviour
     }
 
 
-    // 사운드를 Play 기능하는 함수
+    // 사운드 Play
     public void Play(string name)
     {
         Sound sound = null;
@@ -75,11 +77,64 @@ public class AudioManager : MonoBehaviour
     }
 
 
-    public void StopBgm(string name)
+   // BGM 중지
+    public void StopBgm()
     {
         Sound sound = null;
 
+        foreach (Sound s in sounds)
+        {
+            if (s.sound_Name == bgmName)
+            {
+                sound = s;
+                break;
+            }
+        }
 
+        if (sound == null)
+        {
+            //Debug.Log("Stop Sound : " + name + "File not found!!!");
+            return;
+        }
+
+        bgmName = "";
+        sound.audioSource.Stop();
+    }
+
+    // BGM 플레이
+    public void PlayBgm(string name)
+    {
+        //기존에 플레이 되는 배경음과 새로운 배경음이 같을때
+        if (bgmName == name)
+        {
+            return;
+        }
+
+        //기존 배경음 중단
+        StopBgm();
+
+        //새로운 배경음 플레이
+        Sound sound = null;
+
+        foreach (Sound s in sounds)
+        {
+            if (s.sound_Name == name)
+            {
+                sound = s;
+                break;
+            }
+        }
+
+        if (sound == null)
+        {
+            Debug.Log("Play Sound : " + name + "File not found!!!");
+            return;
+        }
+
+        sound.volume = 0.7f;
+        bgmName = sound.sound_Name;
+
+        sound.audioSource.Play();
     }
 
 
