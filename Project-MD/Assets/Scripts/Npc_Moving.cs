768692a6c10316323615b477a3e5de6d278c3f1f
MooniMakey <80091657+moonimax@users.noreply.github.com>
2023-11-26 22:47:13 +0900

    11

--- Project-MD/Assets/Scripts/Npc_Moving.cs
+++ Project-MD/Assets/Scripts/Npc_Moving.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Npc_Moving : MonoBehaviour
@@ -8,44 +9,46 @@ public class Npc_Moving : MonoBehaviour
     [HideInInspector]
     public float move_speed = 3f;
 
-    public float  startSpeed = 7f;
+   
+    // 생성할 자식 GameObject 프리팹
+    public GameObject childPrefab;
 
     // 도착한 wayPoint 저장 변수
     private int wayPointIndex = 0;
     // 이동할 목표 지점
     private Transform target;
 
+
+    // 이동확정될 wavepoint 저장변수
+    private Transform[] target_wavepoint = new Transform[7];
+
+    private void OnEnable()
+    {
+        // npc의 위치를 초기화시켜야함
+         Vector3 this_character_pos = this.transform.position;
+
+        npc_Build();
+
+        
+    }
+
     private void Start()
     {
+
         // 초기시에 0으로 설정
         wayPointIndex = 0;
 
-        // npc의 위치를 초기화시켜야함
-         Vector3 this_character_pos = this.transform.position;
-        // npc의 위치를 기준으로 wayPoint 0으로 설정
-        WayPoint.points[0].transform.position = this_character_pos;
-         
-        // 생성된 정령값의 위치를 기준으로 0으로 생성하게 해야함
-        target = WayPoint.points[0];
+        //wayPoint =  
     
     }
 
-    private void Update()
-    {
-        // [1] 방향 구하기 - Vector3 : 목표위치 - 현재위치    
-        Vector3 dir = target.position - this.transform.position;
-
-        Debug.Log("바라보고 있는 방향이 :" + dir + " 입니다");
-    }
-
-
-
+    
 
     // 도착시 다음 포인터의 정보를 가져와서 타겟 설정
-    private void GetNexxtPoint()
+    private void GetNextPoint()
     {
         // 종점 판별해서 도착하면 Enemy 오브젝트 Destroy
-        if(wayPointIndex == WayPoint.points.Length)
+       // if(wayPointIndex == WayPoint.points.Length)
         {
             // 새로운 길을 만들어내는 함수
 
@@ -53,9 +56,20 @@ public class Npc_Moving : MonoBehaviour
 
         wayPointIndex++;
         
-        target = WayPoint.points[wayPointIndex];
+        //target = WayPoint.points[wayPointIndex];
 
     }
 
+    public void npc_Build()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            GameObject newChild = Instantiate(childPrefab, this.gameObject.transform); // 부모 오브젝트의 자식 transform 생성
+            Vector3 randomPosition = new Vector3(Random.Range(-5f, 5f), Random.Range(0, 0), Random.Range(-5f, 5f));
+            newChild.transform.position = randomPosition;
+            Debug.Log(i + "번째 위치는 : " + newChild.transform.position);
+
+        }
+    }
 
 }
---
+++ Project-MD/Assets/Scripts/SpawnArea.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+    public GameObject spawn_NPC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newNPC = Instantiate(spawn_NPC, transform);       
+    }
+
+}
